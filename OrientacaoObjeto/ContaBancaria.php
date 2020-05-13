65c62aff3644c52baf1bb49e08c709847b0b7298
Sandro <sandrosa197647@gmail.com>
2020-05-13 12:18:30 -0300

    Introdução do métodos

--- OrientacaoObjeto/ContaBancaria.php
+++ OrientacaoObjeto/ContaBancaria.php
@@ -1,15 +1,84 @@
 <?php
+declare(strict_types=1);
 
 class ContaBancaria {
-  public $banco;
-  private $nomeTitular ='João Pedro';
-  public $numeroAgencia;
-  public $numeroConta;
-  public $saldo;
+  /**
+   * @var string
+   */
+  private $banco;
+  /**
+   * @var string
+   */
+  private $nomeTitular;
+  /**
+   * @var string
+   */
+  private $numeroAgencia;
+  /**
+   * @var string
+   */
+  private $numeroConta;
+  /**
+   * @var 
+   */
+  private $saldo;
+
+  public function __construct(
+    string $banco,
+    string $nomeTitular,
+    string $numeroAgencia,
+    string $numeroConta,
+    float $saldo
+  ){
+    $this->banco = $banco;
+    $this->nomeTitular = $nomeTitular;
+    $this->numeroAgencia = $numeroAgencia;
+    $this->numeroConta = $numeroConta;
+    $this->saldo = $saldo;
+  }
+  public function obterSaldo() : string
+  {
+    return 'O saldo do dia é: R$'.$this->saldo;
+  }
+
+  public function depositar( float $valor) : string
+  {
+    $this->saldo += $valor;
+    return 'Depósito de: R$ '.$valor. ' realizado com sucesso.';
+
+  }
+  public function sacar(float $valor) 
+  {
+    $this->saldo -= $valor;
+    return 'Saque de: R$ '.$valor. ' realizado com sucesso.';
+
+  }
 }
+$conta = new ContaBancaria(
+  'Banco do Brasil', //banco
+  'Sandro Amâncio',  //nome
+  '8455', //numeroAgencia
+  '94730-7',  //numeroConta
+  0 //saldo
+);
 
-// o objeto e a estancia de uma classe
-//Exemplo abaixo está atriuindo objeto a um nova variavel 
-$conta = new ContaBancaria();
+$conta2 = new ContaBancaria(
+  'Caixa Econômica Federal', //banco
+  'Jão Pedro Cardoso',  //nome
+  '2390', //numeroAgencia
+  '98234-5',  //numeroConta
+  0 //saldo
+);
+var_dump($conta);
+var_dump($conta2);
 
-var_dump($conta->nomeTitular);
+// echo $conta->obterSaldo();
+// echo PHP_EOL;
+// echo $conta->depositar(300.40);
+// echo PHP_EOL;
+// echo $conta->obterSaldo();
+// echo PHP_EOL;
+// echo $conta->sacar(150);
+// echo PHP_EOL;
+// echo $conta->obterSaldo();
+// echo PHP_EOL;
